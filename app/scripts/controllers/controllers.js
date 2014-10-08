f144183bbf67fe92fbbeb4804740642fc472d940
blackat <eugenio_lentini@fastmail.fm>
2014-10-08 08:10:25 +0200

    added mongodb support and some fixes

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var movieApp = angular.module('movieApp', ['xeditable', 'ngMockE2E']);
+var movieApp = angular.module('movieApp', ['xeditable']);
 
 movieApp.controller('MainController', function($scope){
 
@@ -27,13 +27,8 @@ movieApp.controller('MovieController', function($scope, $http) {
             $scope.movies = response;
         });
     };
-});
 
-// --------------- mock $http requests ----------------------
-movieApp.run(function($httpBackend) {
-    $httpBackend.whenGET('/movies').respond([
-        {id: 1, title: "Commando", actors: "Arnold Schwarzenegger, Rae Dawn Chong", director: "Mark L. Lester", year: 1985},
-        {id: 2, title: "Raw Deal", actors: "Arnold Schwarzenegger, Kathryn Harrold, Darren McGavin", director: "John Irvin", year: 1986},
-        {id: 3, title: "Predator", actors: "Arnold Schwarzenegger, Carl Weathers, Bill Duke", director: "John McTiernan", year: 1987}
-    ])
+    $scope.saveMovie = function(data, id) {
+        return $http.post("/movie/" + id);
+    }
 });
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 'use strict';
 
 var express = require('express'),
-    app = express();
+    app = express(),
+    path = require('path'),
+    fs = require('fs'),
+    mongoose = require('mongoose');
 
+// Open db connection.
+var environment = require('./lib/config/env/development');
+
+mongoose.connect(environment.mongo.uri);
+// Load mongoose data models.
+var mongooseModels = path.join(__dirname + '/lib/models');
+
+fs.readdirSync(mongooseModels).forEach(function(file){
+    console.log('Load mongoose model: ' + mongooseModels + '/' + file);
+    require(mongooseModels + '/' + file);
+});
+// Populate database with mock data.
+require('./lib/config/mockData');
+
+// Load routes and express configuration.
 require('./lib/routes')(app);
 require('./lib/config/express')(app);
 
+// Start express listening.
 var server = app.listen(9001, function(){
     console.log('Express server is listening on port %d', server.address().port);
 });
